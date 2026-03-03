03e1662d86b81fe990b7e8e7a9095df8b44e9541
Thezzer <miguelarcanjocavallimedeiros@gmail.com>
2026-03-02 21:51:23 -0300

    arrays com classes

---
+++ Array/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Conta[] vetConta = new Conta[3];
+        for(int i = 0; i < vetConta.Length; i++){
+            vetConta[i]= new Conta();
+            Console.WriteLine("Digite o número: ");
+            vetConta[i].numero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o titular:");
+            vetConta[i].titular = Console.ReadLine();
+            Console.WriteLine("Digite o saldo: ");
+            vetConta[i].saldo = Convert.ToDouble(Console.ReadLine());
+        }
+    }
+}
