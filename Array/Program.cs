2b5e240a4a8bd05cefdabfb6222a74f81ac93b51
Thezzer <miguelarcanjocavallimedeiros@gmail.com>
2026-03-14 08:04:43 -0300

    atulizado

--- Array/Program.cs
+++ Array/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Array;
 using System.ComponentModel;
 
 internal class Program
@@ -8,6 +9,8 @@ internal class Program
 
         Conta[] vetConta = new Conta[3];
         for(int i = 0; i < vetConta.Length; i++){
+
+            //instância de cada índice
             vetConta[i]= new Conta();
             Console.WriteLine("Digite o número: ");
             vetConta[i].numero = Convert.ToInt32(Console.ReadLine());
@@ -15,6 +18,13 @@ internal class Program
             vetConta[i].titular = Console.ReadLine();
             Console.WriteLine("Digite o saldo: ");
             vetConta[i].saldo = Convert.ToDouble(Console.ReadLine());
+        }
+        //Some todos os saldos 
+        // e mostre
+
+        foreach (Conta i in vetConta)
+        {
+            
         }
     }
 }
